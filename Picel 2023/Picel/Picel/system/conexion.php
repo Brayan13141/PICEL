051ac1a855d22a919b18780069c9e976a92e202e
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-07-15 13:29:20 -0600

    Cambios minimos para el ajuste de funcionamiento

--- Picel 2023/Picel/Picel/system/conexion.php
+++ Picel 2023/Picel/Picel/system/conexion.php
@@ -3,7 +3,7 @@
     $X00x1 = "root";//USUARIO
     $X00x3 = "root";//CONTRASEÑA
     $X00x4 = "picel3";//BD
-    $link = new mysqli($X00x0, $X00x1, $X00x3,$X00x4);
+    $link = new mysqli($X00x0, $X00x1, $X00x3,$X00x4,'3306');
     $link->set_charset('utf8');
 ?>
 
--- Picel 2023/Picel/Picel/system/login.php
+++ Picel 2023/Picel/Picel/system/login.php
@@ -1,77 +1,68 @@
 <?php
 include("conexion.php");
-include("encryptPSA.php");
-function antiscript($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}    
-if(isset($_POST['usuario']) && !empty($_POST['usuario']) && 
-isset($_POST['pass']) && !empty($_POST['pass']))
+function antiscript($data)
 {
-    
-    //$ip = $_SERVER["REMOTE_ADDR"];
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+if (
+    isset($_POST['usuario']) && !empty($_POST['usuario']) &&
+    isset($_POST['pass']) && !empty($_POST['pass'])
+) {
+
     $keyend = base64_encode("PICEL");
-    $usuario= antiscript($_POST['usuario']);
-    $pass= antiscript($_POST['pass']);
+    $usuario = antiscript($_POST['usuario']);
+    $pass = antiscript($_POST['pass']);
     $usenc = bin2hex($usuario);
-    //$time_regis = time();
-        
+
     $sql = "SELECT * FROM docentes d, usuarios u WHERE d.id_user = u.id_user and u.usuario= UNHEX('$usenc')";
     $complet = $link->query($sql);
+
     $sql2 = "SELECT * FROM estudiantes e, usuarios u WHERE e.id_user = u.id_user and u.usuario= UNHEX('$usenc')";
-    var_dump($complet2);
     $complet2 = $link->query($sql2);
-    if($f=$complet->fetch_array()){         
-        if($pass==$f['contrasena']){
-                session_start();
-                $_SESSION['id_User']=$f['id_User'];
-                $_SESSION['correo']=$f['correo'];
-                $_SESSION['nombre']=$f['nombre'];
-                $_SESSION['apellidoP']=$f['apellidoP'];
-                $_SESSION['apellidoM']=$f['apellidoM'];
-                $_SESSION['tipo_us']=$f['tipo_us'];
-                /*$sql2 = "UPDATE usuarios SET time_log='$time_regis' WHERE usuario='$username'";
-                $link->query($sql2);
-                $sql3 = "UPDATE usuarios SET ip_log='$ipend' WHERE usuario='$username'";
-                $link->query($sql3);
-                $sql3 = "UPDATE usuarios SET activo='1' WHERE usuario='$username'";
-                $link->query($sql3);*/
-                header("Location: ../main/");  
-        }else{
+    
+    if ($f = $complet->fetch_array()) {
+        if ($pass == $f['contrasena']) {
+            session_start();
+            $_SESSION['id_User'] = $f['id_User'];
+            $_SESSION['correo'] = $f['correo'];
+            $_SESSION['nombre'] = $f['nombre'];
+            $_SESSION['apellidoP'] = $f['apellidoP'];
+            $_SESSION['apellidoM'] = $f['apellidoM'];
+            $_SESSION['tipo_us'] = $f['tipo_us'];
+            header("Location: ../main/");
+        } else {
             $error3 = base64_encode("Datos incorrectos");
             echo "<script>location.href='../?error=$error3'</script>";
         }
-    }
-    else if($f=$complet2->fetch_array()){
-        if($pass==$f['contrasena']){
+    } else if ($f = $complet2->fetch_array()) {
+        if ($pass == $f['contrasena']) {
             session_start();
-            $_SESSION['id_User']=$f['id_User'];
-            $_SESSION['correo']=$f['correo'];
-            $_SESSION['nombre']=$f['nombre'];
-            $_SESSION['apellidoP']=$f['apellidoP'];
-            $_SESSION['apellidoM']=$f['apellidoM'];
-            $_SESSION['tipo_us']=$f['tipo_us'];
+            $_SESSION['id_User'] = $f['id_User'];
+            $_SESSION['correo'] = $f['correo'];
+            $_SESSION['nombre'] = $f['nombre'];
+            $_SESSION['apellidoP'] = $f['apellidoP'];
+            $_SESSION['apellidoM'] = $f['apellidoM'];
+            $_SESSION['tipo_us'] = $f['tipo_us'];
             /*$sql2 = "UPDATE usuarios SET time_log='$time_regis' WHERE usuario='$username'";
             $link->query($sql2);
             $sql3 = "UPDATE usuarios SET ip_log='$ipend' WHERE usuario='$username'";
             $link->query($sql3);
             $sql3 = "UPDATE usuarios SET activo='1' WHERE usuario='$username'";
             $link->query($sql3);*/
-            header("Location: ../main/");  
-        }else{
+            header("Location: ../main/");
+        } else {
             $error3 = base64_encode("Datos incorrectos");
             echo "<script>location.href='../?error=$error3'</script>";
         }
-    }
-    else{
+    } else {
         $error3 = base64_encode("Datos incorrectos");
         echo "<script>location.href='../?error=$error3'</script>";
     }
-}else{
+} else {
     $error2 = base64_encode("Campos vacios");
     echo "<script>location.href='../?error=$error2'</script>";
 }
-        
-?>
