34d424fac4f4b866a06101a4566d3cd3acfe4767
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-07-18 11:10:06 -0600

    Sin cambios

---
+++ Picel 2023/Picel/Picel/system/evento-system.php
@@ -0,0 +1,112 @@
+<?php
+include("../system/conexion.php");
+session_start();
+
+function antiscript($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+//----------------------------------CANCELAR-ACTUALIZACION--------------------------------
+if (isset($_POST['cancel']) && $_POST['cancel'] == true) {
+    $_SESSION['editar'] = '';
+    $_SESSION['idEditar'] = '';
+    header('Location: ../main/evento-main.php');
+    exit();
+}
+
+//----------------------------------ACTUALIZAR-ELIMINAR--------------------------------
+if (
+    isset($_POST['accion']) && isset($_POST['id_registro']) &&
+    ($_POST['accion'] == "eliminar_registro" || $_POST['accion'] == "editar_registro")
+) {
+    try {
+        if ($_POST['accion'] == "eliminar_registro") {
+            $id_registro = antiscript($_POST['id_registro']);
+            $BORRAR_EVENTO = "DELETE FROM evento WHERE id_evento=" . $id_registro;
+            if ($link->query($BORRAR_EVENTO)) {
+                echo json_encode(array("success" => true));
+            } else {
+                header('Location: ../main/evento-main.php?mensaje=ERROR AL BORRAR EL EVENTO');
+            }
+        } else {
+            $id_registro = antiscript($_POST['id_registro']);
+            $RESULTADO_EVENTO = "SELECT * FROM evento WHERE id_evento =" . $id_registro;
+            if ($resultado = $link->query($RESULTADO_EVENTO)) {
+                $EVENTO = $resultado->fetch_assoc();
+                $_SESSION['editar'] = true;
+                $_SESSION['idEditar'] =  $id_registro;
+                echo json_encode(array("success" => true, "EVENTO" => $EVENTO));
+            } else {
+                header('Location: ../main/evento-main.php?mensaje=ERROR AL ACTUALIZAR');
+            }
+        }
+    } catch (Exception $e) {
+        // Manejo de excepciones
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+//----------------------------------INSERTAR-ACTUALIZAR--------------------------------
+if (
+    isset($_POST['nombre']) && !empty($_POST['nombre']) &&
+    isset($_POST['periodo']) && !empty($_POST['periodo']) &&
+    isset($_POST['descripcion']) && !empty($_POST['descripcion'])
+) {
+    try {
+        $nombre = antiscript($_POST['nombre']);
+        $periodo = antiscript($_POST['periodo']);
+        $descripcion = antiscript($_POST['descripcion']);
+
+        $sql = "SELECT * FROM evento WHERE id_Evento = '$nombre'";
+        $complet = $link->query($sql);
+
+        $id_User = $_SESSION['id_User'];
+        $sql1 = "SELECT id_Docente FROM docentes WHERE id_User = '$id_User'";
+        $complet2 = $link->query($sql1);
+        $row = $complet2->fetch_assoc();
+        $id_Doc = $row["id_Docente"]; 
+
+        if (mysqli_num_rows($complet) == 0 && is_null(isset($_SESSION['editar'])) && is_null(isset($_SESSION['idEditar']))) {
+            header('Location: ../main/evento-main.php?mensaje=ENTRO AL PRIMER IF');
+            $insert = "INSERT INTO evento(id_Docente,nombre, periodo, descrip) VALUES ('$id_Doc','$nombre', '$periodo', '$descripcion')";
+            if ($link->query($insert)) {
+                header('Location: ../main/evento-main.php?mensaje=EVENTO REGISTRADO');
+            } else {
+                header('Location:../main/evento-main.php?mensaje=ERROR AL REGISTRAR EL EVENTO  ');
+            }
+        } else if (isset($_SESSION['editar']) && $_SESSION['editar'] == true && isset($_SESSION['idEditar'])) {
+            try {
+                
+                $update = "UPDATE evento SET nombre = '$nombre', periodo = '$periodo', descrip = '$descripcion' WHERE id_Evento = " . $_SESSION['idEditar'];
+                if ($link->query($update)) {
+                    $_SESSION['editar'] = '';
+                    $_SESSION['idEditar'] = '';
+                    header('Location: ../main/evento-main.php?mensaje=SE HA ACTUALIZADO CORRECTAMENTE');
+                    exit();
+                } else {
+                    header('Location: ../main/evento-main.php?mensaje=ERROR AL ACTUALIZAR EL EVENTO');
+                    exit();
+                }
+            } catch (Exception $e) {
+                header('Location: ../main/evento-main.php?mensaje=' . $e->getMessage());
+            }
+        }
+    } catch (Exception $e) {
+        header('Location: ../main/evento-main.php?mensaje=ERROR: ' . $e->getMessage());
+    }
+} else {
+    //VALIDACION PARA QUE NO INTERVENGA CON LA FUNCIONALIDAD DE EDITAR Y ELIMINAR
+    if (
+        isset($_POST['accion']) && isset($_POST['id_registro']) &&
+        ($_POST['accion'] == "eliminar_registro" || $_POST['accion'] == "editar_registro")
+    ) {
+        //NO SE REALIZA NADA
+    } else {
+        header('Location: ../main/evento-main.php?mensaje=NO DEJES CAMPOS VACIOS');
+        exit();
+    }
+}
