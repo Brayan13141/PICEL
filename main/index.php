5c7daa5eec7dc775ce0356a3372d107878a70269
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-10-06 21:25:15 -0600

    SE DIO ACCESO A LOS ESTUDIANTES PARA LOS DOCENTES

--- main/index.php
+++ main/index.php
@@ -2,117 +2,129 @@
 session_start();
 if ($_SESSION['id_User']) {
 ?>
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-    <head>
-        <title>PICEL ~ MAIN</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="../ico/logo.ico">
-        <link href="../css/bootstrap.min.css" rel="stylesheet">
-        <link href="../css/estilo.css?v=<?php echo time(); ?>" rel="stylesheet">
-        <!--<script src="https://kit.fontawesome.com/2a6b22e937.js" crossorigin="anonymous"></script>-->
-        <script src="../js/2a6b22e937.js" crossorigin="anonymous"></script>
+<head>
+    <title>PICEL ~ MAIN</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="../ico/logo.ico">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/estilo.css?v=<?php echo time(); ?>" rel="stylesheet">
+    <!--<script src="https://kit.fontawesome.com/2a6b22e937.js" crossorigin="anonymous"></script>-->
+    <script src="../js/2a6b22e937.js" crossorigin="anonymous"></script>
 
-    </head>
-    <style>
-    .col{
-        margin: 5px !important;
-    }
+</head>
+<style>
+.col {
+    margin: 5px !important;
+}
 </style>
-    <body>
-        <?php
+
+<body>
+    <?php
         require('menu.php');
         ?>
-        <section class="container margen">
-            <div class="row mt-4">
-                <div class="col text-center">
-                    <h3>¡Bienvenido <?php echo $_SESSION['nombre'] . ' ' . $_SESSION['apellidoP'] . ' ' . $_SESSION['apellidoM'] . '!'; ?></h3>
-                </div>
+    <section class="container margen">
+        <div class="row mt-4">
+            <div class="col text-center">
+                <h3>¡Bienvenido
+                    <?php echo $_SESSION['nombre'] . ' ' . $_SESSION['apellidoP'] . ' ' . $_SESSION['apellidoM'] . '!'; ?>
+                </h3>
             </div>
-            <div class="row mt-4">
-                <?php
+        </div>
+        <div class="row mt-4">
+            <?php
                 if ($_SESSION['tipo_us'] == "Admin") {
                 ?>
-                    <div class="col">
-                        <div class="card card-main mx-auto" onclick="location.href='rd-main.php';" style="width: 18rem;">
-                            <i class="fa fa-users" aria-hidden="true" style="font-size: 218px;text-align: center;"></i>
-                            <div class="card-body">
-                                <h4 class="text-center"><strong>Docentes</strong></h4>
-                            </div>
-                        </div>
+            <div class="col">
+                <div class="card card-main mx-auto" onclick="location.href='rd-main.php';" style="width: 18rem;">
+                    <i class="fa fa-users" aria-hidden="true" style="font-size: 218px;text-align: center;"></i>
+                    <div class="card-body">
+                        <h4 class="text-center"><strong>Docentes</strong></h4>
                     </div>
-                    <div class="col">
-                        <div class="card card-main mx-auto" onclick="location.href='re-main.php';" style="width: 18rem;">
-                            <i class="fa fa-users" aria-hidden="true" style="font-size: 218px;text-align: center;"></i>
-                            <div class="card-body">
-                                <h4 class="text-center"><strong>Estudiantes</strong></h4>
-                            </div>
-                        </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card card-main mx-auto" onclick="location.href='re-main.php';" style="width: 18rem;">
+                    <i class="fa fa-users" aria-hidden="true" style="font-size: 218px;text-align: center;"></i>
+                    <div class="card-body">
+                        <h4 class="text-center"><strong>Estudiantes</strong></h4>
                     </div>
-                    <div class="col">
-                        <div class="card card-main mx-auto" onclick="location.href='evento-main.php';" style="width: 18rem;">
-                            <i class="fa fa-calendar" aria-hidden="true" style="font-size: 218px;text-align: center;"></i>
-                            <div class="card-body">
-                                <h4 class="text-center"><strong>Eventos</strong></h4>
-                            </div>
-                        </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card card-main mx-auto" onclick="location.href='evento-main.php';" style="width: 18rem;">
+                    <i class="fa fa-calendar" aria-hidden="true" style="font-size: 218px;text-align: center;"></i>
+                    <div class="card-body">
+                        <h4 class="text-center"><strong>Eventos</strong></h4>
                     </div>
-                <?php
+                </div>
+            </div>
+            <?php
                 }
                 ?>
-                <?php
+            <?php
                 if ($_SESSION['tipo_us'] == "Docente" || $_SESSION['tipo_us'] == "Admin") {
                 ?>
-                    <div class="col">
-                        <div class="card card-main mx-auto" onclick="location.href='act-main.php';" style="width: 18rem;">
-                            <i class="fa fa-book " aria-hidden="true" style="font-size: 218px;text-align: center;"></i>
-                            <div class="card-body">
-                                <h4 class="text-center"><strong>Actividades</strong></h4>
-                            </div>
-                        </div>
+            <div class="col">
+                <div class="card card-main mx-auto" onclick="location.href='act-main.php';" style="width: 18rem;">
+                    <i class="fa fa-book " aria-hidden="true" style="font-size: 218px;text-align: center;"></i>
+                    <div class="card-body">
+                        <h4 class="text-center"><strong>Actividades</strong></h4>
                     </div>
-                <?php
+                </div>
+            </div>
+            <div class="col">
+                <div class="card card-main mx-auto" onclick="location.href='re-main.php';" style="width: 18rem;">
+                    <i class="fa fa-users" aria-hidden="true" style="font-size: 218px;text-align: center;"></i>
+                    <div class="card-body">
+                        <h4 class="text-center"><strong>Estudiantes</strong></h4>
+                    </div>
+                </div>
+            </div>
+            <?php
                 }
                 ?>
-                <?php
+            <?php
                 if ($_SESSION['tipo_us'] == "Estudiante" || $_SESSION['tipo_us'] == "Admin") {
                 ?>
-                    <div class="col">
-                        <div class="card card-main mx-auto" onclick="location.href='Actividades_Pantalla_Alumno.php';" style="width: 18rem;">
-                            <i class="fa fa-paper-plane" aria-hidden="true" style="font-size: 218px;text-align: center;"></i>
-                            <div class="card-body">
-                                <h4 class="text-center"><strong>Mis actividades</strong></h4>
-                            </div>
-                        </div>
+            <div class="col">
+                <div class="card card-main mx-auto" onclick="location.href='Actividades_Pantalla_Alumno.php';"
+                    style="width: 18rem;">
+                    <i class="fa fa-paper-plane" aria-hidden="true" style="font-size: 218px;text-align: center;"></i>
+                    <div class="card-body">
+                        <h4 class="text-center"><strong>Mis actividades</strong></h4>
                     </div>
-                <?php
+                </div>
+            </div>
+            <?php
                 }
                 ?>
-            </div>
-        </section>
-    </body>
-    <?php
+        </div>
+    </section>
+</body>
+<?php
     require('footer.php');
     ?>
 
-    </html>
+</html>
 <?php
 } else {
 ?>
-    <HTML>
+<HTML>
 
-    <HEAD>
-        <TITLE>Picel</TITLE>
+<HEAD>
+    <TITLE>Picel</TITLE>
 
-    </HEAD>
+</HEAD>
 
-    <body BGCOLOR="black">
-        <script>
-            location.href = "../";
-        </script>
-    </body>
+<body BGCOLOR="black">
+    <script>
+    location.href = "../";
+    </script>
+</body>
 <?php
 }
 ?>
