38494aae0dacf61d0464b7262f5a12f45169daac
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-10-07 22:20:05 -0600

    SE AGREGO LA CONSULTA PARA QUE SE RECUPEREN Y SE MUESTREN TODAS LAS ACTIVIDADES DE EL ALUMNO

--- main/Actividades-Alumnos.php
+++ main/Actividades-Alumnos.php
@@ -1,13 +1,14 @@
 <?php
 session_start();
-if (isset($_SESSION['tipo_us']) != "Admin" || isset($_SESSION['tipo_us']) != "Docente") {
+/*if (isset($_SESSION['tipo_us']) != "Admin" || isset($_SESSION['tipo_us']) != "Docente") {
   header('Location: ../main/index.php');
-}
+}*/
 if (isset($_SESSION['tipo_us']) && isset($_SESSION['id_User'])) {
-  include('../system/conexion.php');
+    include('../system/conexion.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Actividades</title>
     <meta charset="UTF-8">
@@ -18,10 +19,11 @@ if (isset($_SESSION['tipo_us']) && isset($_SESSION['id_User'])) {
     <script src="../js/jquery-3.6.0.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
 </head>
+
 <body>
     <?php
-    require('menu.php');
-    ?>
+        require('menu.php');
+        ?>
     <div class="container margen">
         <div class="row" style="display:block;visibility:visible;" id="registros">
             <div class="col-12 pt-2 text-center">
@@ -29,28 +31,57 @@ if (isset($_SESSION['tipo_us']) && isset($_SESSION['id_User'])) {
             </div>
             <div class="grid mx-auto col-md-12">
                 <?php
-                $query = "SELECT DISTINCT id_Evento FROM actividades_asignadas";
-                echo '<table id="tabReg" class="table table-hover mt-3" border="0" cellspacing="2" cellpadding="2"> 
-              <tr> 
-                  <td><p>Evento</p></td> 
-                  <td><p>No. Estudiantes</p></td> 
-                  <td><p>Docente</p></td> 
-                  <td><p>Estatus</p></td> 
-                  <td><p>Detalles</p></td>  
-              </tr>';
-                echo "</table>";
-                ?>
+                    $query = "SELECT DISTINCT t.Nombre,t.descripcion,concat(d.nombre,' ',d.apellidoP,' ',d.apellidoM) 
+                    AS nombre_completo, t.estatus, t.anotaciones
+                    FROM tarea t, actividades_asignadas a, docentes d
+                    WHERE a.id_Actividades = t.id_Actividad
+                    AND a.id_Docente = d.id_Docente
+                    AND t.id_Estudiante = '" . $_SESSION['num_control'] . "'";
+
+
+
+                    echo '<table id="tabReg" class="table table-hover mt-3" border="0" cellspacing="2" cellpadding="2"> 
+                    <tr> 
+                        <td><strong><p>Tarea</p></strong></td> 
+                        <td><strong><p>Descripcion</p></strong></td> 
+                        <td><strong><p>Docente</p></strong></td>
+                        <td><strong><p>Estatus</p></strong></td>
+                        <td><strong><p>Anotaciones</p></strong></td>
+                        
+                    </tr>';
+
+                    if ($result = $link->query($query)) {
+                        while ($row = $result->fetch_assoc()) {
+                            $field1name = $row["Nombre"];
+                            $field2name = $row["descripcion"];
+                            $field3name = $row["nombre_completo"];
+                            $field4name = $row["estatus"];
+                            $field5name = $row["anotaciones"];
+                            echo' <tr>
+                                <td><p>' . $field1name . '</p></td>
+                                <td><p>' . $field2name . '</p></td>
+                                <td><p>' . $field3name . '</p></td>
+                                <td><p>' . $field4name . '</p></td>
+                                <td><p>' . $field5name . '</p></td>
+                            </tr>';
+                        }
+                        $result->free();
+                    }
+                    echo "</table>";
+
+                    ?>
+
             </div>
         </div>
     </div>
     <footer>
         <?php
-        include('footer.php');
-        ?>
+            include('footer.php');
+            ?>
     </footer>
 </body>
 
-</html> 
+</html>
 <?php
 }
 ?>
