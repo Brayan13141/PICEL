6c46f5875514312dc692dd18a091e2b77c253ee0
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-10-07 22:34:42 -0600

    SE MODIFICO LA BD PARA AGREGAR EL CAMPO DE LOS DOCENTES A LOS ESTUDIANTES PARA MOSTRAR SOLO LOS QUE SON DE CADA DOCENTE

--- main/re-main.php
+++ main/re-main.php
@@ -3,17 +3,18 @@ session_start();
 if (isset($_SESSION['tipo_us']) != "Admin") {
   header("Location: index.php");
 }
-if ((isset($_SESSION['id_User']) && $_SESSION['tipo_us'] =="Admin") ||
-(isset($_SESSION['id_User']) && $_SESSION['tipo_us'] =="Docente")) {
+if ((isset($_SESSION['id_User']) && $_SESSION['tipo_us'] == "Admin") ||
+  (isset($_SESSION['id_User']) && $_SESSION['tipo_us'] == "Docente")
+) {
   include('../system/conexion.php');
   $_SESSION['editar'] = '';
   $_SESSION['IdEditar'] = '';
 ?>
-  <!DOCTYPE html>
-  <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
 
-  <head>
+<head>
     <title>ESTUDIANTES</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -24,25 +25,26 @@ if ((isset($_SESSION['id_User']) && $_SESSION['tipo_us'] =="Admin") ||
     <script src="../js/bootstrap.min.js"></script>
     <script src="../main/JS/re-main.js"> </script>
 
-  </head>
+</head>
 
-  <body>
+<body>
     <?php
     require('menu.php');
     ?>
     <div class="container margen">
-      <div class="row mt-3">
-        <div class="col"><button type="button" onclick="load(0);" class="mx-auto d-flex justify-content-center btn btn-success">Ver Registros</button></div>
-        <div class="col"><button type="button" onclick="load(1);" class="mx-auto d-flex justify-content-center btn btn-success">Registrar Estudiante</button></div>
-      </div>
-
-      <div class="row" style="display:none;visibility:hidden;" id="registros">
-        <div class="col-12 pt-2 text-center">
-          <h3>Estudiantes</h3>
+        <div class="row mt-3">
+            <div class="col"><button type="button" onclick="load(0);"
+                    class="mx-auto d-flex justify-content-center btn btn-success">Ver Registros</button></div>
+            <div class="col"><button type="button" onclick="load(1);"
+                    class="mx-auto d-flex justify-content-center btn btn-success">Registrar Estudiante</button></div>
         </div>
-        <div class="grid mx-auto col-md-12">
-          <?php
-          $query = "SELECT * FROM estudiantes";
+
+        <div class="row" style="display:none;visibility:hidden;" id="registros">
+            <div class="col-12 pt-2 text-center">
+                <h3>Estudiantes</h3>
+            </div>
+            <div class="grid mx-auto col-md-12">
+                <?php
           echo '<table class="table table-hover mt-3" border="0" cellspacing="2" cellpadding="2"> 
         <tr> 
               <td><strong><p>No. Control</p></strong></td>
@@ -53,6 +55,12 @@ if ((isset($_SESSION['id_User']) && $_SESSION['tipo_us'] =="Admin") ||
               <td><strong><p>Teléfono</p></strong></td>
               <td><strong><p>Opciones</p></strong></td>
         </tr>';
+
+          if ($_SESSION['tipo_us'] == "Admin") {
+            $query = "SELECT * FROM estudiantes";
+          } else if ($_SESSION['tipo_us'] == "Docente") {
+            $query = "SELECT * FROM estudiantes WHERE id_Docente = (SELECT id_Docente FROM docentes WHERE id_User = " . $_SESSION['id_User'] . ")";
+          }
 
           if ($result = $link->query($query)) {
             while ($row = $result->fetch_assoc()) {
@@ -82,139 +90,143 @@ if ((isset($_SESSION['id_User']) && $_SESSION['tipo_us'] =="Admin") ||
             $result->free();
           }
           ?>
+            </div>
         </div>
-      </div>
 
 
-      <div class="row" style="display:none;visibility:hidden;" id="registrar">
-        <div class="col text-center pt-2 pb-4">
-          <h3>Registro de Estudiantes</h3>
+        <div class="row" style="display:none;visibility:hidden;" id="registrar">
+            <div class="col text-center pt-2 pb-4">
+                <h3>Registro de Estudiantes</h3>
+            </div>
+            <div class="grid mx-auto col-md-8" style="--bs-columns: 2;">
+                <form id="frm-re" method="POST" action="../system/sm_re.php" novalidate>
+                    <div class="row">
+                        <div class="mb-3 col-3">
+                            <label for="nc-c" class="form-label">No. de Control</label>
+                            <input type="text" class="form-control" id="nc-c" name="numerocontrol" required>
+                        </div>
+                        <div class="mb-3 col-3">
+                            <label for="nombre-c" class="form-label">Nombre</label>
+                            <input type="text" class="form-control" id="nombre-c" name="nombre" required>
+                        </div>
+                        <div class="mb-3 col-3">
+                            <label for="apellidoP-c" class="form-label">Apellido Paterno</label>
+                            <input type="text" class="form-control" id="apellidoP-c" name="apellidoP" required>
+                        </div>
+                        <div class="mb-3 col-3">
+                            <label for="apellidoM-c" class="form-label">Apellido Materno</label>
+                            <input type="text" class="form-control" id="apellidoM-c" name="apellidoM" required>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-3 col-6">
+                            <label for="carrera-c" class="form-label">Carrera</label>
+                            <select name="carrera" class="form-control" id="carrera-c" required>
+                                <option value="ING SISTEMAS COMPUTACIONALES">ING SISTEMAS COMPUTACIONALES</option>
+                                <option value="ING AMBIENTAL">ING AMBIENTAL</option>
+                                <option value="ING INDUSTRIAL">ING INDUSTRIAL</option>
+                                <option value="ING GESTION EMPRESARIAL">ING GESTION EMPRESARIAL</option>
+                                <option value="ING ELECTRONICA">ING ELECTRONICA</option>
+                                <option value="ING SISTEMAS AUTOMOTRICES">ING SISTEMAS AUTOMOTRICES</option>
+                                <option value="GASTRONOMIA">GASTRONOMIA</option>
+                            </select>
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label for="correo-c" class="form-label">Correo</label>
+                            <input type="email" class="form-control" id="correo-c" name="correoinstituto" required>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-3 col-6">
+                            <label for="semestre-c" class="form-label">Semestre</label>
+                            <input type="number" class="form-control" id="semestre-c" min="1" max="16" value="1"
+                                name="semestre" required>
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label for="telefono-c" class="form-label">Teléfono</label>
+                            <input type="text" class="form-control" id="telefono-c" name="telefono" required>
+                        </div>
+                    </div>
+                    <div class="row" id="usuarioEstudiante">
+                        <div class="mb-3 col-6">
+                            <label for="usuario-c" class="form-label">Usuario</label>
+                            <input type="text" class="form-control" id="usuario-c" name="usuario" required>
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label for="password-c" class="form-label">Contraseña</label>
+                            <input type="password" class="form-control" id="password-c" name="password" required>
+                        </div>
+                    </div>
+                    <div class="pt-2 d-flex justify-content-around">
+                        <button id="btn-regis" type="submit" class="btn btn-picel">Registrar</button>
+                        <button type="button" class="btn btn-picel-Cancel" onclick="cancel()">Cancelar</button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div class="grid mx-auto col-md-8" style="--bs-columns: 2;">
-          <form id="frm-re" method="POST" action="../system/sm_re.php" novalidate>
-            <div class="row">
-              <div class="mb-3 col-3">
-                <label for="nc-c" class="form-label">No. de Control</label>
-                <input type="text" class="form-control" id="nc-c" name="numerocontrol" required>
-              </div>
-              <div class="mb-3 col-3">
-                <label for="nombre-c" class="form-label">Nombre</label>
-                <input type="text" class="form-control" id="nombre-c" name="nombre" required>
-              </div>
-              <div class="mb-3 col-3">
-                <label for="apellidoP-c" class="form-label">Apellido Paterno</label>
-                <input type="text" class="form-control" id="apellidoP-c" name="apellidoP" required>
-              </div>
-              <div class="mb-3 col-3">
-                <label for="apellidoM-c" class="form-label">Apellido Materno</label>
-                <input type="text" class="form-control" id="apellidoM-c" name="apellidoM" required>
-              </div>
-            </div>
-            <div class="row">
-              <div class="mb-3 col-6">
-                <label for="carrera-c" class="form-label">Carrera</label>
-                <select name="carrera" class="form-control" id="carrera-c" required>
-                  <option value="ING SISTEMAS COMPUTACIONALES">ING SISTEMAS COMPUTACIONALES</option>
-                  <option value="ING AMBIENTAL">ING AMBIENTAL</option>
-                  <option value="ING INDUSTRIAL">ING INDUSTRIAL</option>
-                  <option value="ING GESTION EMPRESARIAL">ING GESTION EMPRESARIAL</option>
-                  <option value="ING ELECTRONICA">ING ELECTRONICA</option>
-                  <option value="ING SISTEMAS AUTOMOTRICES">ING SISTEMAS AUTOMOTRICES</option>
-                  <option value="GASTRONOMIA">GASTRONOMIA</option>
-                </select>
-              </div>
-              <div class="mb-3 col-6">
-                <label for="correo-c" class="form-label">Correo</label>
-                <input type="email" class="form-control" id="correo-c" name="correoinstituto" required>
-              </div>
-            </div>
-            <div class="row">
-              <div class="mb-3 col-6">
-                <label for="semestre-c" class="form-label">Semestre</label>
-                <input type="number" class="form-control" id="semestre-c" min="1" max="16" value="1" name="semestre" required>
-              </div>
-              <div class="mb-3 col-6">
-                <label for="telefono-c" class="form-label">Teléfono</label>
-                <input type="text" class="form-control" id="telefono-c" name="telefono" required>
-              </div>
-            </div>
-            <div class="row" id="usuarioEstudiante">
-              <div class="mb-3 col-6">
-                <label for="usuario-c" class="form-label">Usuario</label>
-                <input type="text" class="form-control" id="usuario-c" name="usuario" required>
-              </div>
-              <div class="mb-3 col-6">
-                <label for="password-c" class="form-label">Contraseña</label>
-                <input type="password" class="form-control" id="password-c" name="password" required>
-              </div>
-            </div>
-            <div class="pt-2 d-flex justify-content-around">
-              <button id="btn-regis" type="submit" class="btn btn-picel">Registrar</button>
-              <button type="button" class="btn btn-picel-Cancel" onclick="cancel()">Cancelar</button>
-            </div>
-          </form>
-        </div>
-      </div>
 
 
-      <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true" style="border: 1px solid black;">
-        <div class="modal-dialog modal-lg">
-          <div class="modal-content">
-            <div class="modal-header" style="background-color: #5fbc18; position: relative;">
-              <div style="position: absolute; left: 10px;">
-                <img src="../imgs/logorecortado.png" class="img-fluid" alt="PICEL" width="55">
-              </div>
-              <h5 class="modal-title w-100 text-center" id="deleteModalLabel">Confirmar Eliminación</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true"
+            style="border: 1px solid black;">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header" style="background-color: #5fbc18; position: relative;">
+                        <div style="position: absolute; left: 10px;">
+                            <img src="../imgs/logorecortado.png" class="img-fluid" alt="PICEL" width="55">
+                        </div>
+                        <h5 class="modal-title w-100 text-center" id="deleteModalLabel">Confirmar Eliminación</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>
+                            ¿Estás seguro de que deseas eliminar a
+                            <strong>
+                                <label id="Nombre-Modal">
+                                </label>
+                            </strong>
+                            ?
+                        </p>
+                        <label>Esta acción no se puede deshacer.</label>
+                    </div>
+                    <div class="modal-footer" style=" background-color: #5fbc18;">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+                            style="border: 1px solid black;">Cancelar</button>
+                        <button type="button" class="btn btn-danger" id="confirmDelete"
+                            style="border: 1px solid black;">Eliminar</button>
+                    </div>
+                </div>
             </div>
-            <div class="modal-body">
-              <p>
-                ¿Estás seguro de que deseas eliminar a
-                <strong>
-                  <label id="Nombre-Modal">
-                  </label>
-                </strong>
-                ?
-              </p>
-              <label>Esta acción no se puede deshacer.</label>
-            </div>
-            <div class="modal-footer" style=" background-color: #5fbc18;">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" style="border: 1px solid black;">Cancelar</button>
-              <button type="button" class="btn btn-danger" id="confirmDelete" style="border: 1px solid black;">Eliminar</button>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
 
     <?php
     if (isset($_GET['mensaje'])) {
     ?>
-      <div class="modal fade" id="ModalMensaje" style="border: 1px solid black;" tabindex="-3">
+    <div class="modal fade" id="ModalMensaje" style="border: 1px solid black;" tabindex="-3">
         <div class="modal-dialog modal-lg">
-          <div class="modal-content">
-            <div class="modal-header" style="background-color: #5fbc18; position: relative;">
-              <div style="position: absolute; left: 10px;">
-                <img src="../imgs/logorecortado.png" class="img-fluid" alt="PICEL" width="55">
-              </div>
-              <h5 class="modal-title w-100 text-center">MENSAJE</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <p id="LabelModal"><?php
+            <div class="modal-content">
+                <div class="modal-header" style="background-color: #5fbc18; position: relative;">
+                    <div style="position: absolute; left: 10px;">
+                        <img src="../imgs/logorecortado.png" class="img-fluid" alt="PICEL" width="55">
+                    </div>
+                    <h5 class="modal-title w-100 text-center">MENSAJE</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p id="LabelModal"><?php
                                   if (isset($_GET['mensaje'])) {
                                     echo $_GET['mensaje'];
                                   } else {
                                     echo "";
                                   }
                                   ?></p>
+                </div>
+                <div class="modal-footer" style=" background-color: #5fbc18;">
+                    <h6>Da clic fuera del mensaje para continuar</h6>
+                </div>
             </div>
-            <div class="modal-footer" style=" background-color: #5fbc18;">
-              <h6>Da clic fuera del mensaje para continuar</h6>
-            </div>
-          </div>
         </div>
-      </div>
+    </div>
     <?php
       echo '<script>
        // Esperamos a que el documento se haya cargado completamente
@@ -229,25 +241,25 @@ if ((isset($_SESSION['id_User']) && $_SESSION['tipo_us'] =="Admin") ||
     ?>
     <?php include('footer.php'); ?>
     </div>
-  </body>
+</body>
 
-  </html>
+</html>
 <?php
 } else {
 ?>
-  <HTML>
+<HTML>
 
-  <HEAD>
+<HEAD>
     <TITLE>Picel</TITLE>
 
-  </HEAD>
+</HEAD>
 
-  <body BGCOLOR="black">
+<body BGCOLOR="black">
     <script>
-      location.href = "../main/index.php";
+    location.href = "../main/index.php";
     </script>
 
-  </body>
+</body>
 <?php
 }
 ?>
